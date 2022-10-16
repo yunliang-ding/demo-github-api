90664cbeafca476498ea2f02d91464872b6b5ff1
yunliang <804408086@qq.com>
2022-10-16 20:44:28 +0800

    fix

--- job128.tsx
+++ job128.tsx
@@ -1,5 +1,26 @@
+/** 定时器 */
+
+const useCountDownTimer = (value) => {
+  const [number, setNumner] = React.useState(value);
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      if(number === 0){
+        return clearInterval(timer);
+      }
+      setNumner(number - 1);
+      return () => {
+        clearInterval(timer);
+      }
+    }, 1000)
+  }, [number]);
+  return [
+    number
+  ]
+}
+
 export default () => {
+  const [number] = useCountDownTimer(3);
   return <div>
-    jjksiiuwieu
+    {number}
   </div>
 }
